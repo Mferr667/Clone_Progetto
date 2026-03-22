ec590a5aac4c71ad45077ace6d19a850e1e28d57
mferr <michael.ferretti@mail.polimi.it>
2026-03-22 10:04:43 +0100

    new

--- progetto/src/TurnOrderCard.java
+++ progetto/src/TurnOrderCard.java
@@ -1,20 +1,4 @@
 import java.util.ArrayList;
 
 public abstract class TurnOrderCard {
-    private ArrayList<Player> playerOrder;
-
-    protected TurnOrderCard(int numPlayers) {
-        this.playerOrder = new ArrayList<>(numPlayers);
-    }
-
-    protected ArrayList<Player> getPlayerOrder() {
-        return playerOrder;
-    }
-
-    protected void setPlayerOrder(Board board){
-
-    }
-
-    protected void resolveBonusOrMalus(){
-    }
 }
